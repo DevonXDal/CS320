be347cc0c540f69b3891ddb1ebf326145bd70ff0
Devon Dalrymple <devonxpersonal@gmail.com>
2022-02-22 00:13:35 -0500

    Added the public interface for the WastePile class

---
+++ DesigningPatienceSolitaire/PatienceSolitaire/src/WastePile.java
@@ -0,0 +1,26 @@
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * This WastePile class represents the waste/discard pile of cards used in Patience Solitaire that receives cards from the
+ * deck and is refilled into the deck.
+ *
+ * Why: This class is different from the other piles because it can have all of its cards removed and returned from it at
+ * once.
+ *
+ * @author Devon X. Dalrymple
+ * @version 2022-02-22
+ */
+public class WastePile {
+
+    /**
+     * Removes and returns all the cards from the waste pile in the order that they were in, in the waste pile (bottom to top).
+     *
+     * Why: This method exists in order to refill the stock/deck with cards when empty like the waste would in the actual game.
+     *
+     * @return The cards from the waste pile from bottom to top
+     */
+    public Collection<Card> removeAll() {
+        return null;
+    }
+}
