1903c19f3ef9cebbff8ce90195f0f9a89d7be88c
Devon Dalrymple <devonxpersonal@gmail.com>
2022-02-24 01:37:33 -0500

    Added the final class, Program, to run the application when ready.

---
+++ DesigningPatienceSolitaire/PatienceSolitaire/src/other/Program.java
@@ -0,0 +1,19 @@
+package other;
+
+/**
+ * This Program class runs the game of Patience Solitaire. Once it creates the GameController it runs the initializeGame method and then waits
+ * for the program to finish.
+ *
+ * Why: This Program class exists to separate the responsibility of starting everything up from the game controller.
+ * This also makes it easier to locate the main method.
+ */
+public final class Program {
+
+    /**
+     * Program entrypoint that sets up and runs the controller to get everything going.
+     * @param args Command line arguments, none are actually used
+     */
+    public static void main(String[] args) {
+
+    }
+}
