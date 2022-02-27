c4721122543f4e03273ae443d1f828baecba42e7
Devon Dalrymple <devonxpersonal@gmail.com>
2022-02-27 15:53:40 -0500

    Finished the MockCommandLine so that testing can begin with the controller.

--- DesigningPatienceSolitaire/PatienceSolitaire/tests/mocks/MockCommandLine.java
+++ DesigningPatienceSolitaire/PatienceSolitaire/tests/mocks/MockCommandLine.java
@@ -4,6 +4,11 @@ import controller.IController;
 import other.Command;
 import view.ICommandLine;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * Pretends to be the command line to a controller. It handles the public API that the controller would call in order to
  * perform its responsibilities.
@@ -12,10 +17,18 @@ import view.ICommandLine;
  * when expected during its testing. Without a mock command line, the controller will be extremely difficult to test correctly.
  *
  * @author Devon X. Dalrymple
- * @version 2022-02-22
+ * @version 2022-02-27
  */
 public class MockCommandLine implements ICommandLine {
     private IController controller;
+
+    // Use the add() method to queue the input to be sent (stop the system by using the 'exit' input)
+    public static LinkedList<Command> mockUserInput = new LinkedList<>(); // Sends input like a queue as requested
+
+    // Use the poll() method to receive lines of input as it came in
+    public static LinkedList<String> systemOutput = new LinkedList<>(); // Receives output like a queue as requested
+
+    public static boolean gameWonMessagePrinted = false; // Will be true if the printWinMessage() method is called
 
     /**
      * Tests the setting up of the controller
@@ -24,26 +37,43 @@ public class MockCommandLine implements ICommandLine {
      */
     @Override
     public void setController(IController controller) {
-
+        this.controller = controller;
     }
 
+    /**
+     * Sends the next fake command and then waits to receive system output strings to store
+     * Send the last command with 'exit' to terminate the controller
+     */
     @Override
     public void collectAndHandleInput() {
-
+        systemOutput.add(controller.sendInput(mockUserInput.poll()));
     }
 
+    /**
+     * Returns the next mock input to the controller to carry out the next action.
+     *
+     * @return The next command to be used with testing
+     */
     @Override
     public Command requestMultiCardMoveDestination() {
-        return null;
+        return mockUserInput.poll();
     }
 
+    /**
+     * Stores the information from the controller instead of printing it to the screen.
+     *
+     * @param updateInformation The text to be printed to stored for testing purposes
+     */
     @Override
     public void printGameUpdate(String updateInformation) {
-
+        systemOutput.add(updateInformation);
     }
 
+    /**
+     * Stores that this method was called
+     */
     @Override
     public void printWinMessage() {
-
+        gameWonMessagePrinted = true;
     }
 }
